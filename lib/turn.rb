6f848a3ffce4d947b05399ec58e77181e1aad331
Jazmin M <jazminmerckel@gmail.com>
2019-09-16 03:13:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,4 @@
-def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
-    return true
-  else 
-    return false
-  end
-end
+
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -21,3 +15,4 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
