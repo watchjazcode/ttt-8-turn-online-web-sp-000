2e15a585bc5df8eeea892b259d7a31896d592a20
Jazmin M <jazminmerckel@gmail.com>
2019-09-16 03:14:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,13 @@ def move(board_array, index, character)
   board_array[index] = character
 end
 
-
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
 
 def valid_move?(board, index)
   if index.between?(0, 8) && !position_taken?(board, index)
